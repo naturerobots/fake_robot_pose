a4608eed11274daf6e26b2a4e036f5de347f95e1
Sebastian Pütz <spuetz@uos.de>
2022-09-01 10:20:49 +0200

    add frequency of 30Hz

--- src/odom_robot_pose.cpp
+++ src/odom_robot_pose.cpp
@@ -10,8 +10,16 @@
 
 boost::shared_ptr<tf2_ros::TransformBroadcaster> broadcaster_ptr;
 size_t seq;
+ros::Time last_call;
+ros::Duration freq;
 
 void odomCallback(const nav_msgs::Odometry::ConstPtr& odom_ptr){
+
+  if(ros::Time::now() - last_call < freq)
+    return;
+  else
+    last_call = ros::Time::now();
+
   tf2::Stamped<tf2::Transform> robot_pose_tmp;
   tf2::fromMsg(odom_ptr->pose.pose, static_cast<tf2::Transform&>(robot_pose_tmp));
   geometry_msgs::TransformStamped robot_pose = tf2::toMsg(robot_pose_tmp);
@@ -27,10 +35,13 @@ int main(int argc, char** argv){
   ros::init(argc, argv, "odom_robot_pose");
   ros::NodeHandle nh;
 
+  // 30 Hz frequency
+  freq = ros::Duration(1.0 / 30.0);
+  last_call = ros::Time::now();
+
   seq = 0;
   broadcaster_ptr = boost::make_shared<tf2_ros::TransformBroadcaster>();
   ros::Subscriber sub = nh.subscribe("gazebo_odom", 100, odomCallback);
-
   ros::spin();
 
 }
